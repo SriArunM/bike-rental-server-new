8e174ec8951ac5ebf332685cf31a7320f63d3e22
SRIARUN MARIMUTHU <113443056+SriArunM@users.noreply.github.com>
2025-04-29 20:09:58 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', configs.CLIENT_URL, 'https://bikerental24.netlify.app/'],
+    origin: ['http://localhost:5173', configs.CLIENT_URL,'https://bikerental24.netlify.app', 'https://bikerental24.netlify.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
